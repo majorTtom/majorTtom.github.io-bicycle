fe3d593751fa505935a8f1b5224f9ab08710e4b2
majorTtom <69448481+majorTtom@users.noreply.github.com>
2020-09-07 16:30:58 +0300

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,19 @@
+<?php
+if (!empty($_POST['user_name']) AND !empty($_POST['user_email']) AND !empty($_POST['user_massage'])){
+
+	$headers = 'From: Абубачир\r\n'. 'Reply-To: jollyollly@gmail.com\r\n'. 'X-Mailer: PHP/'. phpversion();
+
+	$theme = 'Новое сообщение';
+
+	$letter = 'Данные сообщения:\r\n';
+	$letter .='Email: ' .$_POST['user_email']. '\r\n';
+
+
+	if(mail('jollyollly@gmail.com', $theme, $letter, $headers)){
+		header('location:/thankyou.php')
+	} else{
+		header('location:/');
+	}
+} else {
+	header('location:/');
+}
